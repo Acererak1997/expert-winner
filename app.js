cfa1e507342b9abbefdd36729f77b3bfd20a157e
Cthulhu <jaxa5963@gmail.com>
2020-06-01 15:58:16 +0900

    JStask3配列での処理

--- app.js
+++ app.js
@@ -1,50 +1,47 @@
 'use strict'
 
-// 追加
-function addRow() {
-  const table = document.getElementById("table");
-  const row = table.insertRow(-1);
-  
-  let cell1 = row.insertCell(-1);
-  let cell2 = row.insertCell(-1);
-  let cell3 = row.insertCell(-1);
-  let cell4 = row.insertCell(-1);
-  let tIndex = table.rows.length-1;
-  
-  let text = document.getElementById("text").value;
-  cell1.innerHTML = '<span class="index">' + tIndex +'</span>' ; 
-  cell2.innerHTML = text; 
-  cell3.innerHTML = '<button  class="prog" value="作業" onclick="change()">作業中</button>'
-  cell4.innerHTML = '<button id="delete" onclick="deleteRow(this)">削除</button>'; 
-  document.getElementById("text").value = "";
-}
+//追加時の処理
+document.getElementById('add').addEventListener('click', ()=> {
+  let todos = [];
+  let table = document.getElementById("table");
+  const todoText = document.getElementById('text').value;
 
-// リスト削除&番号の更新
-// function deleteRow(todo){
-//   let todoTr = todo.parentNode.parentNode;
-//   let tab = todoTr.parentNode;
-//   if(tab)
-//   tab.deleteRow(todoTr.sectionRowIndex);
+  let todo = {
+    task: '',
+    status: '作業中'
+  }
+  todo.task = todoText;
+  todo.status = '作業中';
+  todos.push(todo);
+  console.log(todos)
 
-//   let indexNumber = document.getElementsByTagName("span");
-//   if (!indexNumber)
-//   return false;
-  
-//   let num = 1;
-//   for (let i = 0; i < indexNumber.length; i++){
-//     if (indexNumber[i].className.match("index"))
-//     indexNumber[i].innerHTML = num++;
-//   }
-// }
+// 最後へ行を追加
+  const addRow = table.insertRow(-1);
 
+// ID
+  const IDcell = addRow.insertCell(-1);
+  const Id = document.createElement('span');
+  Id.textContent =  table.rows.length;
+  IDcell.appendChild(Id);
+// タスク
+  const taskCell = addRow.insertCell(-1);
+  const task = document.createElement('span');
+  task.textContent = todo.task;
+  taskCell.appendChild(task);
 
-// // タスク状態の変更
-// function change(e){
-//   let todo = event.target.innerHTML;
-//   console.log(todo);
-//   if(todo === "作業中"){
-//     todo.document.write = "完了";
-//   }
-// }
+  // 状態
+  const statusCell = addRow.insertCell(-1);
+  const statusbutton = document.createElement("input"); 
+  statusbutton.type = "button";  
+  statusbutton.id = "prog";  
+  statusbutton.value = todo.status; 
+  statusCell.appendChild(statusbutton);
 
-// タブへの表示
+  // 削除ボタン
+  const celldelete = addRow.insertCell(-1);
+  const deletebutton = document.createElement("input");  
+  deletebutton.type = "button";  
+  deletebutton.id = "delete";  
+  deletebutton.value = "削除"; 
+  celldelete.appendChild(deletebutton);
+});
